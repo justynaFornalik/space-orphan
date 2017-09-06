5fcb8ddbbdc1cd3b6e719dece6cfe8083f0f9f81
Joanna Baran <joannabaran56@gmail.com>
2017-09-06 21:59:26 +0200

    Move fight with boss function back to space_orphan.py

--- fight.py
+++ fight.py
@@ -1,9 +1,6 @@
 import random
-import os
 
-import printing
-import hot_game
-from constants import *
+from constants import MONSTERS
 
 
 def fight_monster(monster, health_points, experience_points):
@@ -18,18 +15,3 @@ def fight_monster(monster, health_points, experience_points):
     input('Press enter to continue: ')
 
     return health_points, experience_points
-
-
-def fight_with_boss(health_points, experience_points):
-    if experience_points > EXPERIENCE_REQUIRED_TO_FIGHT_BOSS:
-        os.system('clear')
-        result = hot_game.game()
-        if result:
-            printing.print_screen(printing.win_screen)
-            printing.print_hall_of_fame_screen()
-        else:
-            printing.print_screen(printing.lose_screen)
-        printing.print_info_about_authors()
-    else:
-        print("You don't have enough experience. Come back when you are worthy!")
-        time.sleep(3)
--- space_orphan.py
+++ space_orphan.py
@@ -1,15 +1,12 @@
-
-import random
-import time
 import os
-
+import sys
 
 import printing
 import boards
 import inv
 import move
 import fight
-
+import hot_game
 
 from constants import *
 
@@ -23,33 +20,77 @@ def initialise_player():
     return player_row, player_col, health_points, experience_points, inventory
 
 
+def is_not_enough_uranium(inventory):
+    return URANIUM not in inventory or inventory[URANIUM] < NUM_URANIUM_REQUIRED
+
+
+def say_not_enough_uranium():
+    print("You do not have enough uranium to go to the next level!")
+    input("Press enter to continue: ")
+
+
+def is_enough_experience(experience_points):
+    return experience_points > EXPERIENCE_REQUIRED_TO_FIGHT_BOSS
+
+
+def say_not_enough_experience():
+    print("You do not have enough experience to fight The Great Space Hippopotamus!")
+    print("Come back when you are worthy!")
+    input("Press enter to continue: ")
+
+
+def fight_with_boss():
+    if is_enough_experience(experience_points):
+        os.system('clear')
+        result = hot_game.game()
+        if result:
+            printing.print_screen(printing.win_screen)
+        else:
+            printing.print_screen(printing.lose_screen)
+        end_game()
+    else:
+        say_not_enough_experience()
+
+
+def end_game():
+    printing.print_ending_screens()
+    os.system('clear')
+    sys.exit()
+
+
 def play_level(board, player_row, player_col, health_points, experience_points, inventory):
-    while health_points > 0:
+    while True:
         printing.print_board(board, player_row, player_col)
         key = move.getch()
-        if key in MOVE_KEYS:
+
+        if key == ' ':
+            inv.display_inventory(inventory)
+
+        elif key in MOVE_KEYS:
             player_row, player_col = move.move_player(board, player_row, player_col, key)
             printing.print_board(board, player_row, player_col)
+            symbol = board[player_row][player_col]
 
-            if board[player_row][player_col] in ITEMS:
-                item = board[player_row][player_col]
-                inv.add_to_inventory(inventory, item)
-                inv.display_inventory(inventory)
+            if symbol in ITEMS:
+                inv.add_to_inventory(inventory, symbol)
                 board[player_row][player_col] = EMPTY_SPACE
 
-            elif board[player_row][player_col] in MONSTERS:
-                monster = board[player_row][player_col]
-                health_points, experience_points = fight.fight_monster(monster, health_points, experience_points)
+            elif symbol in MONSTERS:
+                health_points, experience_points = fight.fight_monster(symbol, health_points, experience_points)
+                board[player_row][player_col] = EMPTY_SPACE
                 if health_points <= 0:
                     printing.print_screen(printing.lose_screen)
-                    printing.print_info_about_authors()
-                board[player_row][player_col] = EMPTY_SPACE
+                    end_game()
 
-            elif board[player_row][player_col] == BOSS:
-                result = fight.fight_with_boss(health_points, experience_points)
+            elif symbol == WAY_TO_SECOND_LEVEL:
+                if is_not_enough_uranium(inventory):
+                    say_not_enough_uranium()
+                else:
+                    break
 
-        elif key == ' ':
-            inv.display_inventory(inventory)
+            elif symbol == BOSS:
+                fight_with_boss()
+
     return health_points, experience_points, inventory
 
 
@@ -57,6 +98,13 @@ def main():
     printing.print_starting_screens()
     board = boards.initialise_board('board_level_1.txt')
     player_row, player_col, health_points, experience_points, inventory = initialise_player()
+    health_points, experience_points, inventory = play_level(board, player_row, player_col,
+                                                             health_points, experience_points, inventory)
+
+    player_row, player_col = 1, 1
+    board = boards.initialise_board('board_level_2.txt')
     play_level(board, player_row, player_col, health_points, experience_points, inventory)
 
-main()
+
+if __name__ == '__main__':
+    main()
