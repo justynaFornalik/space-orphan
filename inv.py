b0390a79c25936f9feba54049ab631b3292e7bb9
Joanna Baran <joannabaran56@gmail.com>
2017-09-06 15:04:06 +0200

    Really Fix display inventory function to deal with empty inventory

--- inv.py
+++ inv.py
@@ -1,13 +1,6 @@
-SPACE_WATER = 'W'
-SPACE_BANANA = 'B'
-SPACE_PIE = 'P'
-URANIUM = 'U'
+import time
 
-ITEMS = {SPACE_WATER: ['space water', 'food', 1],
-         SPACE_BANANA: ['space banana', 'food', 1],
-         SPACE_PIE: ['space pie', 'food', 1],
-         URANIUM: ['uranium', 'material', 1]}
-HEADLINE = ' '*10 + 'NAME' + ' ' * 15 + 'TYPE'+' ' * 10 + 'WEIGHT'+' '*10 + 'QUANTITY'
+from constants import *
 
 
 def add_to_inventory(inventory, item):
@@ -18,16 +11,21 @@ def add_to_inventory(inventory, item):
     return inventory
 
 
-def display_inventory(inventory, ITEMS):
+def display_inventory(inventory):
     table_bar = []
     table_bar.append("-"*70)
     print("Your space inventory:")
-    print(HEADLINE)   
+    headline = ' '*10 + 'NAME' + ' ' * 15 + 'TYPE'+' ' * 10 + 'WEIGHT'+' '*10 + 'QUANTITY'
+    print(headline)
     print("".join(table_bar))
 
-    for item in ITEMS:
-        print((ITEMS[item][0]).rjust(15), (ITEMS[item][1]).rjust(15), str(ITEMS[item][2]).rjust(15), str(inventory[item]).rjust(15))
-    print("".join(table_bar))
-    return inventory
-
-
+    if inventory:
+        for item in ITEMS:
+            if item in inventory:
+                print((ITEMS[item][0]).rjust(15), (ITEMS[item][1]).rjust(15),
+                      str(ITEMS[item][2]).rjust(15), str(inventory[item]).rjust(15))
+        print("".join(table_bar))
+    else:
+        print('Empty inventory')
+    input("Press enter to continue: ")
+    
