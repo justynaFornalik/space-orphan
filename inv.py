3a7e6dd9674597803c3ab83fb6a7e7f29e018629
Joanna Baran <joannabaran56@gmail.com>
2017-09-07 12:07:19 +0200

    Modify inventory to add health points when player picks up food

--- inv.py
+++ inv.py
@@ -1,17 +1,17 @@
 import time
 
-from constants import ITEMS
+from constants import ITEMS, URANIUM
 
 
 def display_inventory(inventory):
     table_bar = []
     table_bar.append("-"*70)
-    print("Your space inventory:")
     headline = ' '*10 + 'NAME' + ' ' * 15 + 'TYPE'+' ' * 10 + 'WEIGHT'+' '*10 + 'QUANTITY'
-    print(headline)
-    print("".join(table_bar))
 
     if inventory:
+        print("Your space inventory:")
+        print(headline)
+        print("".join(table_bar))
         for item in ITEMS:
             if item in inventory:
                 print((ITEMS[item][0]).rjust(15), (ITEMS[item][1]).rjust(15),
@@ -22,11 +22,14 @@ def display_inventory(inventory):
     input("Press enter to continue: ")
 
 
-def add_to_inventory(inventory, item):
+def add_to_inventory(inventory, item, health_points):
     if item in inventory.keys():
         inventory[item] = 1 + inventory[item]
     else:
         inventory[item] = 1
+    if item != URANIUM:
+        health_points += 3
     print(ITEMS[item][0].capitalize(), 'was added to your inventory.')
+    print('You gained 3 health points.')
     display_inventory(inventory)
-    return inventory
+    return health_points
